e3d86e968c6a7d44968a17c2a23a97409ad7a4b1
sam2much96 <sam2much96@gmail.com>
2021-09-05 11:25:33 +0100

    new file:   .import/App's white paper v1.1.6.webp-d3d7c9a30beaa1781ab2b21ec0be0364.md5 	new file:   .import/App's white paper v1.1.6.webp-d3d7c9a30beaa1781ab2b21ec0be0364.stex 	new file:   .import/Cover 1.webp-dab8966c83476d0b972a00b887f4a238.md5 	new file:   .import/Cover 1.webp-dab8966c83476d0b972a00b887f4a238.stex 	new file:   .import/UI hover buttons.webp-47f90888773ebe44130ca4ba0c0042fb.md5 	new file:   .import/UI hover buttons.webp-47f90888773ebe44130ca4ba0c0042fb.stex 	new file:   .import/chap 1 cover page.webp-d3d568760f53095fbbd9605df567663a.md5 	new file:   .import/chap 1 cover page.webp-d3d568760f53095fbbd9605df567663a.stex 	new file:   .import/chap 6 cover art 1_2.webp-316d3b182b04b015909c5b93141040b7.md5 	new file:   .import/chap 6 cover art 1_2.webp-316d3b182b04b015909c5b93141040b7.stex 	new file:   App's white paper v1.1.6.webp 	new file:   App's white paper v1.1.6.webp.import 	new file:   New game code and features/web browser.tscn 	new file:   resources/Buttons/UI hover buttons.webp 	new file:   resources/Buttons/UI hover buttons.webp.import 	new file:   scenes/Comics/chapter 5/chap 1 cover page.webp 	new file:   scenes/Comics/chapter 5/chap 1 cover page.webp.import 	new file:   scenes/Comics/chapter 6/chap 6 cover art 1_2.webp 	new file:   scenes/Comics/chapter 6/chap 6 cover art 1_2.webp.import 	new file:   scenes/Comics/chapter 7/Cover 1.webp 	new file:   scenes/Comics/chapter 7/Cover 1.webp.import 	new file:   scenes/UI & misc/HSlider.gd 	new file:   scenes/UI & misc/anime_streaming.gd 	new file:   scenes/UI & misc/comics-reader-extension.gd 	new file:   scenes/UI & misc/generic_joystick_ui_selector.gd 	new file:   scenes/UI & misc/web_browser.gd 	new file:   scenes/UI & misc/xml.gd 	new file:   scenes/UI & misc/xml.tscn

---
+++ scenes/UI & misc/anime_streaming.gd
@@ -0,0 +1,235 @@
+extends Control
+
+export(String, FILE, "*.webm") var vid_file = ""
+
+var downloading_video = false
+
+var dicL1 = {}
+onready var video_file = File.new()
+onready var dir = Directory.new()
+var download_video_size = 1
+var percent
+onready var error_code = 0
+onready var videoplayer = get_node("VideoPlayer")
+
+func _on_Button_pressed():
+	get_tree().change_scene_to((Globals.title_screen))
+
+
+
+
+
+func _on_watch_anime_pressed():
+#use URL: https://youtu.be/ETtpGXDwe08 to test video streaming.
+#Original youtube amv url: https://youtu.be/sh0ygItcpBg
+	#video_file = File.new()
+
+	######The Streaming Site passed as a global string variable#########
+	if Globals.os != str ('Android'): #Webm doesn't play on Godot v3.2.3 Videoplayer yet
+		Networking.url = 'https://animationvideosondemand.s3.af-south-1.amazonaws.com/shots_1-8+pencil+test.webm' #for debuging video streaming
+		#Networking.url = 'https://animationvideosondemand.s3.af-south-1.amazonaws.com/AMV_3.webm'
+	if Globals.os == str ('Android'):
+		Networking.url ='https://animationvideosondemand.s3.af-south-1.amazonaws.com/shots_1-8+pencil+test.ogv' #OGV
+	
+	
+	dir.open ("user://")
+	var file_exists
+	if Globals.os != str ('Android'):
+		file_exists = dir.file_exists('user://video.webm')
+	if Globals.os == str ('Android'):
+		file_exists = dir.file_exists('user://video.ogv')
+	
+	print ('Video File Exists: ', file_exists)
+
+	
+	if not file_exists:
+		print ('Video File Doesn.t exist,downloading' )#;_check_download_size(int(Networking.get_body_size()), Networking.get_downloaded_bytes())
+		Networking.request(Networking.url)
+		play_loading_cinematic() #Plays the Loading cinematic while the video file downloads
+		downloading_video = true
+		Networking.connect("request_completed", self, "_http_request_completed")
+		print ('download completed')
+
+	if file_exists: 
+		print ('Video File Exists')
+		stop_playing_laoding_cinematic()
+		downloading_video = false
+		var err
+		if Globals.os != str ('Android'):
+			video_file.open("user://video.webm", File.READ_WRITE)
+			err = (video_file.open('user://video.webm', File.READ))
+			print ('Video file is open: ',video_file.is_open(), '/error :', err) #Debugs if file can open
+		if Globals.os == str('Android'):
+			video_file.open("user://video.ogv", File.READ_WRITE)
+			err = (video_file.open('user://video.ogv', File.READ))
+			print ('Video file is open: ',video_file.is_open(), '/error :', err) #Debugs if file can open
+		
+		var video_file_path = video_file.get_path_absolute()
+		print ('Video File Path: ',video_file_path)
+		print('Video file size : ', video_file.get_len())
+		
+		
+		if video_file.get_len() ==0 :
+			push_error('Video file is corrupted')
+		
+		if video_file.is_open() && err == 0: #error catcher 2
+			if Globals.os != str('Android'):
+				Globals.AMV = ResourceLoader.load(video_file_path, "VideoStreamWebm", false)
+			if Globals.os == str ('Android'):
+				Globals.AMV = ResourceLoader.load(video_file_path, 'VideoStreamTheora', false)
+		#Music.notification(NOTIFICATION_PREDELETE) #dreaks the code. Fix Music off function
+			print ('Playing Global video File: ', Globals.AMV )
+			Video_Stream((Globals.AMV), Music.playlist_one[4]) #Play the video with Shootback
+
+
+func _check_download_size(loaded,total): #Kinda works. Sort this code out first
+	if downloading_video == true:
+		if download_video_size == 0 or loaded == 0 or total == 0: #Error catcher 2
+			#print ('Download video size:/', download_video_size, 'Loaded:/',loaded,'Total:/',total) #for debug purposes
+			total = 1
+		if download_video_size != null && total != 0 : # Error catcher 1
+			percent = (loaded)/total
+			#while percent != 100:
+			print('Downloading.../ ', percent, '%')
+
+		if loaded == total and Globals.AMV != null:
+			print (' Download Completed') 
+	if downloading_video == false:
+		pass
+
+
+func _process(_delta):
+	#for debug purposes only
+	#if downloading_video == true:
+	#	yield(get_tree().create_timer(5), "timeout")
+	#	print (_check_download_size(int(Networking.get_body_size()), Networking.get_downloaded_bytes())) #shows a progress report on video being downloaded)
+	pass
+
+###########my codes###############parsing the poopbyte array as a video stream########
+func _http_request_completed(result, response_code, headers, body):
+	#Sets the video stream to the videoplayer
+	#video_file = File.new()
+	
+	if body.empty() != true: #Executes once a Connection is established 
+		
+		
+		
+		dir.open ("user://")
+		var file_exists = dir.file_exists('user://video.webm')
+		print ('Video File Exists: ', file_exists)
+		
+		#Checks if video file exits
+		if not file_exists : #executes if videofile doesnt exit
+			#video_file = File.new()
+			
+			#dir = Directory.new()
+			dir.open("user://")
+			var _absolute_path = dir.get_current_dir ( )
+			
+			print ('Directory //', _absolute_path)
+			var err
+			if Globals.os != str ('Android'):
+				video_file.open('user://video.webm',File.WRITE)
+			#OS.execute (str(_absolute_path), 'chmod + x str(_absolute_paths)', false, 'making folder executable', false)
+				err = (video_file.open('user://video.webm', File.WRITE_READ))
+			if Globals.os == str ('Android'):
+				video_file.open('user://video.ogv',File.WRITE)
+				err = (video_file.open('user://video.ogv', File.WRITE_READ))
+			print ('Video file is open: ',video_file.is_open(), '/error :', err) #Debugs if file can open
+			if video_file.is_open() && err == 0: #error catcher 2
+				#var _body = body
+				
+				
+				download_video_size = Networking.get_body_size()#8gets video size from servers
+				_check_download_size(int(Networking.get_body_size()), Networking.get_downloaded_bytes())
+				#var parser = _body.decompress(download_video_size) #decompresses the poolbyte
+				video_file.store_buffer(body) #store pool byte array as video buffer
+				var video_file_path = video_file.get_path_absolute() #gets the file path
+				Globals.AMV = video_file_path
+				video_file.close()
+				#print('1: Body /' + _body.get_string_from_ascii())#, '2',_r.get_string_from_utf8()) #The second ckind of decoding doesn't work
+				#print ('2: Parser /' , parser)
+				#print ('Parsing Stream','1/' ,_body,' through ', '2/',parser )
+				#print ('video file as a global: ',Globals.video_stream ) #for debug purposes only
+
+
+				
+				downloading_video = false
+				#vid_file = video_file
+				#video_file.open("user://video.webm", File.READ)
+				#var _q2 = video_file.get_path_absolute()
+				#print (_q2)
+				#Music.notification(NOTIFICATION_PREDELETE) #Shuts down Bckgrnd music. This code breaks. Fix music off
+				#Video_Stream(load(video_file_path), Music.playlist_one[4])
+				#print (str(video_file.get_modified_time(video_file))) #gets the last time file was modified
+				#print (video_file.get_path_absolute())
+				#print (vid_file)
+				#Globals.video_stream = video_file #make it a global variable
+
+			#Prints the kind of data gotten from the http request for debugging
+
+			#return Globals.video_stream
+		if file_exists:
+			print ('File Exists', file_exists)
+	if body.empty() == true:
+		print ('Streaming Site '+ Networking.url+ ' is unavailable ')
+		print ('It could be a myriad of problems. Please debug carefully')
+
+
+#to use: video_html('video', body)
+
+func Video_Stream(stream, audio): #Video Streamer
+	videoplayer._set_size((get_viewport_rect().size))
+	if stream!= null or '':
+		print('Playing Video Stream:/',stream)
+		videoplayer.set_stream(stream) 
+		videoplayer.play() 
+		print ('Video player is playing: ',videoplayer.is_playing())
+		
+		#Sets the Audio track to play
+		get_node("AudioStreamPlayer").set_stream(load(audio)) #plays the audio
+		get_node("AudioStreamPlayer").play(0.0)
+		#print ('analysis:' + "It works")
+
+func play_loading_cinematic(): #A simple loading loop
+	_check_download_size(int(Networking.get_body_size()), Networking.get_downloaded_bytes()) #shows a progress report on video being downloaded
+	var _z = str( 'Downloading Remaining.../ ', percent ,'%') #formats the data
+	Dialogs.show_dialog( _z, 'Admin') #displays the download percent to the users
+
+	Video_Stream(Globals.cinematics, Music.wind_sfx[0])
+	
+	print ('Playing loading Cinematic ')
+	yield(get_tree().create_timer(5), "timeout") #Runs this loop every 5 secs
+	
+	if Globals.os != str ("Android") && Globals.AMV == null:
+		if not dir.file_exists("user://video.webm"): #breaks loop
+			play_loading_cinematic()
+			print ('looping loading cinematic')
+		if dir.file_exists("user://video.webm"):
+			stop_playing_laoding_cinematic()
+
+
+	if Globals.os == str ('Android'):
+		if not dir.file_exists("user://video.ogv") && Globals.AMV == null: #breaks loop
+			play_loading_cinematic()
+		print ('looping loading cinematic')
+		if dir.file_exists("user://video.ogv"):
+			stop_playing_laoding_cinematic()
+
+func stop_playing_laoding_cinematic():
+	Dialogs.hide_dialogue() #Error catcher 1 
+
+func debug_stream():
+	print ('vid_stream_debug: ',vid_file)
+	
+
+
+
+
+func exit(error) -> void:
+	print ('Error code: ', error)
+	get_tree().quit()
+
+
+func _on_VideoPlayer_finished(): #Stops Music Once Video Finishes
+	get_node("AudioStreamPlayer").stop()
---
+++ scenes/UI & misc/comics-reader-extension.gd
@@ -0,0 +1,74 @@
+extends HBoxContainer
+
+
+
+onready var loaded_comics 
+
+func _ready():
+	get_node("chap_1").grab_focus()
+	pass
+
+
+func _input(event):
+	'JoyStick Controls'
+	#Cancel event 
+	if event is InputEventJoypadButton :
+		if event.is_action_pressed("ui_cancel"):
+			Globals._go_to_title()
+
+
+
+#Shows Comics Chapters Buttons
+func _on_Comics_loaded_comics():
+	loaded_comics = true
+	for child in get_children(): # works
+		child.hide()
+
+
+#Hides Comics Chapter Buttons 
+func _on_Comics_freed_comics():
+	loaded_comics = false
+	for child in get_children(): 
+		child.show()
+
+#Button controls back
+func _on_Back_button_pressed():
+	if loaded_comics == false or Globals.comics != null: #&& Globals.comics.enabled == false :
+		Globals._go_to_title()
+	if loaded_comics == true:
+		$"/root/Dialogs".show_dialog('Finish Comics First ', 'Admin')
+		yield(get_tree().create_timer(1.5), "timeout")
+		$"/root/Dialogs".dialog_box.hide()
+
+
+
+func _exit_tree(): #resets everything to default
+	OS.set_window_size(Vector2(1280, 720))
+	OS.set_screen_orientation (0)
+	pass
+
+
+
+
+
+#Button controls chap_6
+
+func display_locked_text(): #Dis the dialogue noxplays a generic text through
+	Dialogs.show_dialog('Extra Chapters locked ', 'Admin')
+	yield(get_tree().create_timer(1), "timeout")
+	Dialogs.hide_dialogue()
+
+
+
+func _on_chap_4_pressed():
+	display_locked_text()
+
+
+func _on_chap_5_pressed():
+	display_locked_text()
+
+func _on_chap_6_pressed():
+	display_locked_text()
+
+func _on_chap_7_pressed():
+	display_locked_text()

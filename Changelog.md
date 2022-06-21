3d40bfc729790c406284d8a2fe3f7ef89e5b7927
sam2much96 <sam2much96@gmail.com>
2022-06-21 08:06:02 +0100

    new file:   .gitignore 	new file:   Changelog.md 	new file:   LICENSE 	new file:   _config.yml 	new file:   app/.gitignore 	new file:   app/build.gradle 	new file:   app/proguard-rules.pro 	new file:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/io/cgisca/godot/gpgs/ConnectionController.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/PlayGameServicesGodot.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/accountinfo/PlayerInfoController.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/accountinfo/PlayerInfoListener.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/achievements/AchievementsController.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/achievements/AchievementsListener.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/events/EventsController.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/events/EventsListener.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/leaderboards/LeaderBoardsListener.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/leaderboards/LeaderboardsController.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/model/Achivemements.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/model/PlayerInfo.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/model/PlayerStats.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/savedgames/SavedGamesController.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/savedgames/SavedGamesListener.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/signin/SignInController.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/signin/SignInListener.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/signin/UserProfile.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/stats/PlayerStatsController.kt 	new file:   app/src/main/java/io/cgisca/godot/gpgs/stats/PlayerStatsListener.kt 	new file:   resources/bandit_spritesheet.webp 	new file:   resources/bandit_spritesheet.webp.import 	new file:   scenes/characters/Bandits.tscn 	new file:   scenes/items/enemy spawner.gd 	new file:   scenes/items/enemy spawner.tscn

---
+++ Changelog.md
@@ -0,0 +1,20 @@
+# Changelog
+
+### 1.3.0
+Build for Godot 3.4
+
+### 1.2.0
+Added posibility to enable/disable saved games functionality.
+Fixed bug in saved games functionality.
+
+### 1.1.0
+Fix crash when trying to sign in
+
+### 1.0.0
+Implemented Google play games services features:
+- Sign-in/Sign out
+- Achievements
+- Leaderboards
+- Events
+- Player Stats
+- Saved Games
---
+++ scenes/items/enemy spawner.gd
@@ -0,0 +1,48 @@
+# *************************************************
+# godot3-Dystopia-game by INhumanity_arts
+# Released under MIT License
+# *************************************************
+# Enemy Spawner
+# Spawns enemy instances Within the Scene Tree
+# Features
+# (1) Spawns 12 enemies and turns off once the ememy count is at 3
+# (2) Plays a flames animation once Enemy node is bieng instanced
+# To Do:
+#(1) Spawn different enemy types
+# (2) Expand code's functionaliy
+# *************************************************
+
+extends Position2D
+
+
+export (bool) var enabled 
+export (PackedScene) var enemy_spawn_1
+onready var position_in_area = self.position #origin point
+#var enemy = load('res://scenes/characters/Bandits.tscn') 
+
+
+export(int) var spawn_count = -1
+
+func _ready():
+	randomize()
+	print ('spawning enemy...')
+	if enemy_spawn_1 != null:
+		spawn_enemy()
+
+
+
+func spawn_enemy(): 
+	if spawn_count <= 12 && enabled == true:
+		spawn_count += 1
+
+		#spawn an object in the position
+		var spawn = enemy_spawn_1.instance()
+		$AnimationPlayer.play("spawning")
+		spawn.position = position_in_area
+		get_parent().call_deferred('add_child', spawn)
+	elif spawn_count >= 12:
+		return
+
+func _on_enemy_spawner_timeout():
+	spawn_enemy()
+	pass # Replace with function body.

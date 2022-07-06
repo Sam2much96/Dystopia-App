ebb53a60604b06e0c96b55c40e3b8010ada163a7
sam2much96 <sam2much96@gmail.com>
2022-07-06 15:45:35 +0100

    modified:   addons/algodot/gdnative/gdnative_lib.tres 	deleted:    addons/monero/godot-monero.gd 	modified:   addons/monero/monero.gdns 	modified:   addons/monero/plugin.gd 	modified:   project.godot 	New game code and features/algodot.gd 	New game code and features/crypto_testing.tscn 	New game code and features/godot-monero.gd 	addons/monero/monero.gdnlib

--- addons/monero/plugin.gd
+++ addons/monero/plugin.gd
@@ -18,10 +18,10 @@ func _enter_tree():
 	)
 	
 	# Driver for executing async rust code in _process
-	# add_autoload_singleton("AsyncExecutorDriver", "res://addons/algodot/gdnative/async_executor.gdns")
+	# 
 
 
 func _exit_tree():
 	# Clean up
 	remove_custom_type("monero")
-	# remove_autoload_singleton("AsyncExecutorDriver")
+	# 

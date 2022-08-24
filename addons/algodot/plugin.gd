4d1e6ea380526a8076f0967166d5a8a5fb0afeb5
Sam2much96 <Sam2much96>
2022-08-24 05:59:26 +0100

    modified:   addons/algodot/plugin.gd

--- addons/algodot/plugin.gd
+++ addons/algodot/plugin.gd
@@ -1,29 +1,21 @@
 tool
 extends EditorPlugin
 
-var gdn
 
-#########for built in documentation
 var _editor_view
 
 func _init():
 	add_autoload_singleton('DocsHelper', "res://addons/algodot/Documentation/Scripts/DocsHelper.gd")
-	#####for buiilt in documentation
+	#*********For Built in Documentation**********#
 	
 	_add_custom_editor_view()
 	
 	"Adds a Custom Tab for Documentations"
-	#get_editor_interface().get_editor_viewport().add_child(_editor_view)
-	
-	get_editor_interface().get_inspector().get_parent_control().get_parent().add_child(_editor_view)
-	
-	
-
+	get_editor_interface().get_editor_viewport().add_child(_editor_view)
+	make_visible(false)
 
 func _enter_tree():
-	# Algod custom type
 
-	# Get the base icon from the current editor interface/theme
 	var gui = get_editor_interface().get_base_control()
 	var node_icon = gui.get_icon("Node", "EditorIcons")
 
@@ -33,15 +25,7 @@ func _enter_tree():
 		preload("res://addons/algodot/gdnative/algod.gdns"),
 		node_icon
 	)
-	
-	# Driver for executing async rust code in _process
 	add_autoload_singleton("AsyncExecutorDriver", "res://addons/algodot/gdnative/async_executor.gdns")
-	
-
-	
-	
-
-	
 
 
 
@@ -50,7 +34,8 @@ func _exit_tree():
 	
 	remove_custom_type("Algod")
 	remove_autoload_singleton("AsyncExecutorDriver")
-##################For builtin Documentation
+
+#************For Builtin Documentation***********#
 	
 	remove_autoload_singleton("DocsHelper")
 	_remove_custom_editor_view()
@@ -58,12 +43,12 @@ func _exit_tree():
 
 
 
-###########For Builtin Documentation###################
-func get_plugin_name():
+#***********For Builtin Documentation***********#
+func get_plugin_name()-> String:
 	return "Algodot"
 
 
-func _add_custom_editor_view(): # for builtin documentatiion
+func _add_custom_editor_view(): 
 	_editor_view = preload("res://addons/algodot/Documentation/Scripts/DocumentationViewer.tscn").instance()
 
 	
@@ -71,6 +56,15 @@ func _add_custom_editor_view(): # for builtin documentatiion
 
 func _remove_custom_editor_view():
 	if _editor_view:
-		
-		remove_control_from_docks(_editor_view)
-#		_editor_view.queue_free()
+		_editor_view.queue_free()
+
+func has_main_screen()-> bool:
+	return true
+
+func make_visible(visible: bool) -> void:
+	if _editor_view:
+		_editor_view.visible=visible
+
+func get_plugin_icon()-> Texture:
+	return get_editor_interface().get_base_control().get_icon("Spatial", "EditorIcons")
+

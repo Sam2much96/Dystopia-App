44c92790475322b1be8c12e9d65b0d44ee7f31dd
sam2much96 <sam2much96@gmail.com>
2023-09-24 11:58:00 +0100

    Llama2 API

--- source_code/New game code and features/online_leaderboard.gd
+++ source_code/New game code and features/online_leaderboard.gd
@@ -15,14 +15,14 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Get Request
 func _get_highscores(): #Gets the top 5 highscore killcount as a dictionary
 	pass
 
+# Post Request
 func _save_highscores(): #Saves your highscore and your name to the server
 	pass
-	
+
+# Show 
 func _display_highsocres():
 	pass

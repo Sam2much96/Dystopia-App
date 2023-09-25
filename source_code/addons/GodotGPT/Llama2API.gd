58c0d4593b6047dde94c9ebc35c39b9643630fb9
sam2much96 <sam2much96@gmail.com>
2023-09-25 09:41:47 +0100

    Llama2 api selfhosting

--- source_code/addons/GodotGPT/Llama2API.gd
+++ source_code/addons/GodotGPT/Llama2API.gd
@@ -26,34 +26,55 @@ var _headers = PoolStringArray()
 
 
 func _enter_tree():
-	# Define the API endpoint and headers
-	url = "https://api.replicate.com/v1/predictions"
+	"""
+	REPLICATE API
+	"""
+	print_debug(replicate_api)
 	
-	# Append the Authorization header
-	_headers.append("Authorization: Token " + replicate_api)
+	if replicate_api != null:
+		
+		# Define the API endpoint and headers
+		url = "https://api.replicate.com/v1/predictions"
+		
+		# Append the Authorization header
+		_headers.append("Authorization: Token " + replicate_api)
 
-	# Append the Content-Type header
-	_headers.append("Content-Type: application/json")
+		# Append the Content-Type header
+		_headers.append("Content-Type: application/json")
 
 
-	# Define the data for the POST request
-	data = {
-		"version": "8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e", #7B parameter
-		"input": {
-			"prompt": prompt 
+		# Define the data for the POST request
+		data = {
+			"version": "8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e", #7B parameter
+			"input": {
+				"prompt": prompt 
+			}
 		}
-	}
-
+	
+	"""
+	MY AWS API
+	"""
+	# Repo : https://github.com/Sam2much96/llama2web/blob/main/main.py
+	if replicate_api == null:
+		url = "http://ec2-51-20-53-10.eu-north-1.compute.amazonaws.com:8080/v1/"
+		#_headers.append('Accept : application/json')
+		#_headers.append('Content-Type : application/json')
+		
+		data = {"prompt": prompt,'msg': null, 'type': "dict"}
 
 
 func _ready():
-	# POST method
-	_prompt.connect("request_completed",self, "_request_callback")
+	if replicate_api != null:
+		# For making direct api calls to replicate api
+		# POST method
+		_prompt.connect("request_completed",self, "_request_callback")
 	
+		# GET method
+		response2.connect("request_completed", self, "_output")
 	
-	# GET method
-	response2.connect("request_completed", self, "_output")
-	
+	if replicate_api == null:
+		# POST method
+		_prompt.connect("request_completed",self, "_output")
 
 
 
@@ -118,8 +139,11 @@ func _output(result, response_code, headers, body) -> String:
 func send_prompt(request: HTTPRequest) -> void:
 
 
-	if prompt != "":
+	if prompt != "" :
 		request.request(url,_headers , false, HTTPClient.METHOD_POST, JSON.print(data))
+	#if replicate_api == null:
+	#	request.request(url,_headers , false, HTTPClient.METHOD_POST, JSON.print(data))
+
 
 
 

dbbf31e922d235255a4b66bb837eb58c1d0e0442
sam2much96 <sam2much96@gmail.com>
2023-11-11 13:43:21 +0100

    deleted:    source_code/.import/rotate_screen_webp.webp-59e13dfbf174773eb2f1ffb5050d76e6.stex 	new file:   source_code/.import/rotate_screen_webp2.webp-5383514593fc1b33e46b3c12b86cbc42.md5 	new file:   source_code/.import/rotate_screen_webp2.webp-5383514593fc1b33e46b3c12b86cbc42.stex 	new file:   source_code/resources/misc/PadIcon.tscn 	deleted:    source_code/resources/misc/rotate_screen_webp.webp 	new file:   source_code/resources/misc/rotate_screen_webp2.webp 	renamed:    source_code/resources/misc/rotate_screen_webp.webp.import -> source_code/resources/misc/rotate_screen_webp2.webp.import 	modified:   source_code/scenes/UI & misc/Comics____2.tscn 	modified:   source_code/scenes/UI & misc/Help.gd 	modified:   source_code/scenes/UI & misc/Help.tscn

--- source_code/scenes/UI & misc/Help.gd
+++ source_code/scenes/UI & misc/Help.gd
@@ -14,6 +14,52 @@ extends Control
 
 class_name Help
 
-func _ready():
-	pass
+# UI
+onready var rotate_page : Label = $"Control/GridContainer/HBoxContainer/Label"
+onready var _direction : Label = $"Control/GridContainer/HBoxContainer2/Label"
+onready var _lives : Label = $"Control/GridContainer/HBoxContainer3/Label"
+onready var dash : Label = $"Control/GridContainer/HBoxContainer4/Label"
+onready var _attack : Label = $"Control/GridContainer/HBoxContainer5/Label"
+onready var _interact : Label = $"Control/GridContainer/HBoxContainer6/Label"
+onready var _stats : Label = $"Control/GridContainer/HBoxContainer7/Label"
+onready var _comics : Label = $"Control/GridContainer/HBoxContainer8/Label"
+onready var _zoom : Label = $"Control/GridContainer/HBoxContainer9/Label"
 
+onready var mobile_help : Array = [
+	rotate_page, _direction, _lives, dash, _attack,
+	_interact, _stats, _comics, _zoom
+]
+
+onready var pc_help : Array = []
+
+enum {MOBILE, PC}
+
+export (int) var _state = MOBILE
+
+func _ready():
+	print_debug("Help: ", show_help(Globals.os))
+
+func show_help(os : String) -> int :
+	# Nested IF's?
+	# Bad Code Alert
+	if not os.empty(): # Error Catcher 1
+		if os == "Android" or "iOS":
+			_state = MOBILE
+		else:
+			_state = PC
+	return _state
+	if os.empty(): # Error Catcher 2
+		push_error("Global OS cannot be Empty")
+
+func _process(delta):
+	# Help State Machine
+	match _state:
+		MOBILE:
+			pass
+		PC:
+			pass
+
+
+func _exit_tree():
+	# Memory Management
+	Utils.MemoryManagement.queue_free(mobile_help)

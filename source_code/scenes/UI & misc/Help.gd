17148fdff2a23ef945541c3718dcfc418938ec14
sam2much96 <sam2much96@gmail.com>
2023-11-11 08:01:29 +0100

    modified:   source_code/project.godot 	deleted:    source_code/resources/misc/controls.png~ 	new file:   source_code/scenes/UI & misc/Help.gd 	new file:   source_code/scenes/UI & misc/Help.tscn

---
+++ source_code/scenes/UI & misc/Help.gd
@@ -0,0 +1,19 @@
+# *************************************************
+# godot3-Dystopia-game by INhumanity_arts
+# Released under MIT License
+# *************************************************
+# Shows Noob Players Help
+#
+# Features:
+# (1) Contains UI for Mobile,PC, Console
+# (2) Features Translations
+#
+# *************************************************
+ 
+extends Control
+
+class_name Help
+
+func _ready():
+	pass
+

37780a660febd30f5fa898eda7d7132bd2e6cf5c
sam2much96 <sam2much96@gmail.com>
2024-02-07 15:07:36 +0100

    modified:   source_code/scenes/Title screen.tscn 	modified:   source_code/scenes/UI & misc/Stats.tscn 	modified:   source_code/scenes/UI & misc/minimap.gd

--- source_code/scenes/UI & misc/minimap.gd
+++ source_code/scenes/UI & misc/minimap.gd
@@ -31,33 +31,35 @@ func _process(delta):
 	if (total_delta > 0.5):
 		total_delta -= 0.5
 		update()
+	
 
 # Draw the players on the minimap
 func _draw():
-
-	var peer_id =  get_tree().get_network_unique_id()
-	var pos = Vector2(0,0)
-	if node_root.player_info.has(peer_id):
-		pos.x += node_root.player_info[peer_id].position.x
-		pos.y += node_root.player_info[peer_id].position.y
-		label_position.text = str(round(pos.x))+","+str(round(pos.y))
+	 # Draw Call Called Every Frame
+	# Disabling for debugging
+	#var peer_id =  get_tree().get_network_unique_id()
+	#var pos = Vector2(0,0)
+	#if node_root.player_info.has(peer_id):
+	#	pos.x += node_root.player_info[peer_id].position.x
+	#	pos.y += node_root.player_info[peer_id].position.y
+	#	label_position.text = str(round(pos.x))+","+str(round(pos.y))
+	#	
+	#for _peer_id in node_root.player_info: #i changed peer_id to _peer_id#inhumanity
+	#	
+	#	var object = node_root.player_info[peer_id]
+	#	if object.destroyed:
+	#		continue
+	#		
+	#	# Convert world size to texture rectangle size
+	#	var _p = get_node("/root/Networking").WORLD_SIZE#Networking.WORLD_SIZE
+	#	#print(_p)
+	#	var world_radius = _p / 2
+	#	var x = (object.node.position.x - pos.x) / (world_radius / rect_size.x / 2)
+	#	var y = (object.node.position.y - pos.y) / (world_radius / rect_size.y / 2)
+	#	
+	#	# out of bound, dont render
+	#	if x < -63 or y < -63 or x > 63 or y > 63: continue
+	#	draw_texture(texture_object, Vector2(63 + x,63 + y))
 		
-	for _peer_id in node_root.player_info: #i changed peer_id to _peer_id#inhumanity
-		
-		var object = node_root.player_info[peer_id]
-		if object.destroyed:
-			continue
-			
-		# Convert world size to texture rectangle size
-		var _p = get_node("/root/Networking").WORLD_SIZE#Networking.WORLD_SIZE
-		#print(_p)
-		var world_radius = _p / 2
-		var x = (object.node.position.x - pos.x) / (world_radius / rect_size.x / 2)
-		var y = (object.node.position.y - pos.y) / (world_radius / rect_size.y / 2)
-		
-		# out of bound, dont render
-		if x < -63 or y < -63 or x > 63 or y > 63: continue
-		draw_texture(texture_object, Vector2(63 + x,63 + y))
-		
-	draw_texture(texture_player, Vector2(62,62))
- 
+	#draw_texture(texture_player, Vector2(62,62))
+	pass

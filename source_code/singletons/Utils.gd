7c17a6ac3a88a7a73922553098f28bb854afba69
sam2much96 <sam2much96@gmail.com>
2024-08-06 18:28:46 +0100

    Fix Type Checks and Vector Returns in Utils.gd

--- source_code/singletons/Utils.gd
+++ source_code/singletons/Utils.gd
@@ -20,9 +20,9 @@
 extends Node
 
 
-var screenOrientation : int
-var viewport_size : Vector2
-var center_of_viewport : Vector2 
+@export var screenOrientation : int
+@export var viewport_size : Vector2
+@export var center_of_viewport : Vector2 
 
 @export var EnemyObjPool : Array = [] #Stores shared pointer to enemy Mob instances
 
@@ -603,7 +603,20 @@ class Screen  :
 			#print (scroller.scroll_vertical )#= scroll_constant  * delta
 	
 	
-	static func calculate_button_positional_data(menu, _interract,stats, roll, slash, comics, joystick, D_pad)-> Array:
+	static func calculate_button_positional_data(
+		menu : TouchScreenButton, 
+		_interract : TouchScreenButton,
+		stats : TouchScreenButton, 
+		roll : TouchScreenButton, 
+		slash : TouchScreenButton, 
+		comics : TouchScreenButton, 
+		joystick : TouchScreenButton, 
+		D_pad : Control
+		)-> Array:
+		
+		# OS Check
+		assert(Globals.os == "Android")
+		
 		# Returns an Array containing the position of all Touch HUD items
 		# Only Used in Mobile devices for adjusting TOuchscreen HUD
 		# Rewrite as Static function under utils screen class (Done)
@@ -806,16 +819,16 @@ func check_files(path_to_dir: String, path_to_file : String)-> bool:
 
 
 		# Updates the raycast to the Enemy"s Direction
-static func rotate_pointer(point_direction: Vector2, pointer) -> void:
+static func rotate_pointer(point_direction: Vector2, pointer : Node2D) -> void:
 	var temp =rad_to_deg(atan2(point_direction.x, point_direction.y))
 	pointer.rotation_degrees = temp
 
 
 
-func restaVectores(v1, v2): #vector substraction
+func restaVectores(v1 : Vector2, v2 : Vector2) -> Vector2: #vector substraction
 	return Vector2(v1.x - v2.x, v1.y - v2.y)
 
-func sumaVectores(v1, v2): #vector sum
+func sumaVectores(v1 : Vector2, v2 : Vector2) -> Vector2: #vector sum
 	return Vector2(v1.x + v2.x, v1.y + v2.y)
 
 

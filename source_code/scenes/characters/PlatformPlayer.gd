00299e0ac57f5146a6b5eb3513102f190693c92f
sam2much96 <inhumanityarts@gmail.com>
2026-01-21 17:08:58 +0100

    maintenance update 6

--- source_code/scenes/characters/PlatformPlayer.gd
+++ source_code/scenes/characters/PlatformPlayer.gd
@@ -13,22 +13,24 @@
 # (1) Wall Jumps
 # (2) Implement Player Animations
 # (3) Implement Facing
+# (4) Add despawning code for falling off platforms
 # *************************************************
 
 extends Player
 
 class_name Player_v3_Platformer
 
-@export (int) var speed = 10
-@export (int) var jump_speed = -1800
-@export (int) var gravity = 4000
+@export var speed : int = 10
+@export var jump_speed : int = -1800
+@export var gravity : int = 4000
 
 const MAX_SPEED = 1000
 # For Jumping Mechanics
 const max_air_jumps : int = 6
 var air_jump_counter : int = 0
 
-@export (Vector2) var velocity = Vector2.ZERO
+func _ready():
+	velocity  = Vector2.ZERO
 
 
 
@@ -95,3 +97,6 @@ func _physics_process(delta):
 			await get_tree().create_timer(0.5).timeout
 			gravity = 4000 # Reset Gravity
 	
+	#despawn if falling off overworld sidescrolling mapmap
+	if position.y > 2300:
+		get_tree().change_scene_to_file("res://scenes/levels/overworld_1.tscn")
